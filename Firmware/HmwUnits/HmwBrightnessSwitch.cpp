5db89076b77a989a2bdc68fb58ad4109924ede21
loetmeister <loetmeister@habmalnefrage.de>
2023-03-25 12:08:49 +0100

    new config options: lowest sample, moving average, last sample

--- Firmware/HmwUnits/HmwBrightnessSwitch.cpp
+++ Firmware/HmwUnits/HmwBrightnessSwitch.cpp
@@ -18,8 +18,8 @@ HmwBrightnessSwitch::HmwBrightnessSwitch( HmwAnalogIn& _linkedAnalogChannel, Con
    index( 0 ),
    count( 0 )
 {
-   nextActionDelay = 180;	// 18 seconds delay (allow time for analog sampling)
-   currentValue = 0;
+   nextActionDelay = 180;    // 18 seconds delay (allow time for analog sampling)
+   currentValue = 255;   // start "disabled" and wait until data arrives
    stateFlags.byte = 0;
 }
 
@@ -31,59 +31,61 @@ uint8_t HmwBrightnessSwitch::get( uint8_t* data )
    *data = stateFlags.byte;
    return 2;
 }
-	
+
 void HmwBrightnessSwitch::set( uint8_t length, uint8_t const* const data )
 {
-	// will be called by peered key channel (motion sensor)
-	
-	if ( count && length >= 6 )
-	{
-		triggered = false;
-		stateFlags.element.active = false;
-		
-		switch ( data[0] )
-		{
-			case HmwLed::TOGGLE:	// X LE COND_VALUE
-				if ( currentValue <= data[2] )//actionParameter->onLevel )
-				{
-					triggered = true;
-					stateFlags.element.le = true;
-				}
-				break;
-			case HmwLed::BLINK_ON:	// X GE COND_VALUE
-				if ( currentValue >= data[2] )//actionParameter->onLevel )
-				{
-					triggered = true;
-					stateFlags.element.le = false;
-				}
-				break;
-			//case HmwLed::BLINK_TOGGLE:	// not used
-				//break;
-		}
-		
-		if ( triggered )
-		{
-			stateFlags.element.active = true;
-			
-			if ( data[5] )  //blinkQuantity //actionParameter->blockingTime )
-			{
-			// pause brightness calculation. A trigger received during this time would restart the blocking time and send a key event (by HmwBrightnessKey)
-				nextActionDelay = (uint16_t)data[5] *100;  // 10 - 2550 seconds (nextActionDelay * 100 in main loop!)
-				lastActionTime.setNow();
-				stateFlags.element.blockingTimeActive = true;
-			}
-			else {
-				stateFlags.element.blockingTimeActive = false;
-			}
-		}
-	}
+    if ( count && length >= 6 )    // will be called by peered key channel (motion sensor)
+    {
+        triggered = false;
+        stateFlags.element.active = false;
+        
+        switch ( data[0] )
+        {
+            case HmwLed::TOGGLE:    // X LE COND_VALUE
+                if ( currentValue <= data[2] )  //actionParameter->onLevel
+                {
+                    triggered = true;
+                    stateFlags.element.le = true;
+                }
+                break;
+            case HmwLed::BLINK_ON:    // X GE COND_VALUE
+                if ( currentValue >= data[2] )  //actionParameter->onLevel
+                {
+                    triggered = true;
+                    stateFlags.element.le = false;
+                }
+                break;
+            //case HmwLed::BLINK_TOGGLE:    // not used
+                //break;
+        }
+        
+        if ( triggered )
+        {
+            stateFlags.element.active = true;
+            
+            if ( data[5] )  //blinkQuantity //actionParameter->blockingTime )
+            {
+            // pause brightness calculation. A trigger received during this time would restart the blocking time and send a key event (by HmwBrightnessKey)
+                nextActionDelay = (uint16_t)data[5] *100;  // 10 - 2550 seconds (nextActionDelay * 100 in main loop!)
+                lastActionTime.setNow();
+                stateFlags.element.blockingTimeActive = true;
+            }
+            else {
+                stateFlags.element.blockingTimeActive = false;
+            }
+        }
+    }
+    else if ( length == 1 && data[0] <= 200 )  // allow to set brightness value manually. Set calculation config to MANUAL_VALUE to keep value
+    {
+        currentValue = data[0];
+    }
 }
 
 void HmwBrightnessSwitch::loop()
 {
-   if ( !nextActionDelay )	// not used value, returns >100% (TODO: test if this is good solution...)
+   if ( !config->getInterval() )  // sample interval == 0 disables the channel
    {
-      currentValue = 255;  // also used to tell HmwBrightnessKey this channel is disabled
+      currentValue = 255;  // also tell HmwBrightnessKey this channel is disabled
       return;
    }
 
@@ -97,35 +99,56 @@ void HmwBrightnessSwitch::loop()
    if ( stateFlags.element.blockingTimeActive )
    {
       stateFlags.element.blockingTimeActive = false;
-      nextActionDelay = (uint16_t)config->interval* 100;	// restore configured measurement interval
+      nextActionDelay = config->getInterval();    // restore configured measurement interval
       return;
    }
 
 
    uint8_t reading = linkedAnalogChannel->currentValue / 20;
-   if (reading > 200) reading = 200;	// limit to 100% (200 / 2 = 100%)
+   if (reading > 200) reading = 200;    // limit to 100% (200 / 2 = 100%)
    
-   //TODO: option to use lowest sample, rather than average?
    
    /* calculate the (moving) average of the last n results */
    result_sum -= buffer[index];  // subtract old value
    buffer[index] = reading;  // update buffer with current reading
    result_sum += buffer[index];  // add new value
-   index++;
-   index = index % MAX_SAMPLES; // reset when last array element was processed
+
    if (count < MAX_SAMPLES) count++;  // avoid to calculate the average on array elements that have not been updated with a reading
    
-   if (result_sum > 0)
-	currentValue = result_sum / count;  // calculate average
-   else
-	currentValue = 0;
+   switch (config->getBehaviour())
+   {
+      case Config::Behaviour::LOWEST_SAMPLE:
+      {
+         uint8_t _index = count;
+         uint8_t idxLowest = 0;
+         while (_index) {
+            _index--;
+            if (buffer[_index] <= buffer[idxLowest]) idxLowest = _index;
+         }
+         currentValue = buffer[idxLowest];
+         break;
+      }
+      case Config::Behaviour::AVERAGE:
+         if (result_sum > 0)
+            currentValue = result_sum / count;  // calculate average
+         else
+            currentValue = 0;
+      break;
+      case Config::Behaviour::LAST_SAMPLE:
+         currentValue = buffer[index];
+      break;
+      case Config::Behaviour::MANUAL_VALUE: // do not overwrite currentValue, manually set
+      break;
+   }
+
+   index++;
+   index = index % MAX_SAMPLES; // reset when last array element was processed
 }
 
 void HmwBrightnessSwitch::checkConfig()
 {
    // start next calculation after x seconds
    // min. 10s (larger than analogIn sample interval), up to 450 seconds (gives 1 hour with 8 samples)
-   if (config->interval == 255) config->interval = 14;  // factor 10! default 140 seconds
-   
-   nextActionDelay = (uint16_t)config->interval* 100;	// nextActionDelay * 100 in main loop!
+   config->checkOrRestore();
+   nextActionDelay = config->getInterval();    // nextActionDelay * 100 in main loop!
 }
--- Firmware/HmwUnits/HmwBrightnessSwitch.h
+++ Firmware/HmwUnits/HmwBrightnessSwitch.h
@@ -21,46 +21,68 @@
 #include <xEeprom.h>
 
 
-class HmwAnalogIn;	// forward declare this class
+class HmwAnalogIn;    // forward declare this class
 
 class HmwBrightnessSwitch : public HmwChannel
 {
    public:
-
-      struct Config
+      class Config
       {
-         uint8_tx unused;
-		 // TODO: add uint8_tx behaviour:2;  // 3=lowest sample; 2=moving average; 1=last sample; 0=oldest sample? highest value?
-         uint8_tx interval;
+          struct Options
+          {
+              uint8_t behaviour : 2;  // 3=lowest sample (default); 2=moving average; 1=last sample; 0=maual set value (not reading sensor value)
+              uint8_t reserved : 6;
+          };
+
+          XEeprom<Options> options;
+          uint8_tx interval;
+
+          public:
+
+          enum Behaviour
+          {
+              MANUAL_VALUE = 0,
+              LAST_SAMPLE,
+              AVERAGE,
+              LOWEST_SAMPLE
+          };
+
+          Options getOptions() const
+          {
+              return options.operator*();
+          }
+
+          inline uint8_t getBehaviour() const
+          {
+              return getOptions().behaviour;
+          }
+          
+          inline uint16_t getInterval() const
+          {
+              return (uint16_t)interval *100;
+          }
+          
+          inline void checkOrRestore()
+          {
+              if ( getOptions().reserved != 63 )
+              {
+                  Options defaultOptions =
+                  {
+                      .behaviour = LOWEST_SAMPLE,
+                      .reserved = 63,
+                  };
+                  options = defaultOptions;
+              }
+              if ( interval == 0xFF )
+              {
+                  interval.update( 0x0E );
+              }
+          }
       };
 
       static const uint8_t MAX_SAMPLES = 8;
 
-	// using peering layout of HmwLed!	//TOD: add as struct? HBWLinkLed works differently to dimmer...
-      //struct ActionParameter
-      //{
-	      ////uint32_t sensorAddress;
-	      ////uint8_t sensorChannel;
-	      ////uint8_t ownChannel;
-	      ////uint8_t shortActionType : 3;
-	      ////uint8_t longActionType : 3;
-		  //uint8_t onOffLevelOrCmd;
-	      //uint8_t offLevel;
-	      //uint8_t onLevel;
-	      ////uint8_t longOnLevel;
-	      ////uint8_t longOffLevel;
-	      //uint8_t blinkOnTime;
-	      //uint8_t blinkOffTime;
-	      //uint8_t blockingTime;
-	      ////uint8_t reserved[2];
-      //};
-	  //
-      //struct LinkCommand
-      //{
-	      ////uint8_t keyNum;
-	      //ActionParameter* actionParameter;
-      //};
-	  
+      
       ////    Constructors and destructors    ////
 
       HmwBrightnessSwitch( HmwAnalogIn& _linkedAnalogChannel, Config* _config );
@@ -69,9 +91,9 @@ class HmwBrightnessSwitch : public HmwChannel
 
       // definition of needed functions from HBWChannel class
       virtual uint8_t get( uint8_t* data );
-	  virtual void set( uint8_t length, uint8_t const* const data );
+      virtual void set( uint8_t length, uint8_t const* const data );
       virtual void loop();
-	  virtual void checkConfig();
+      virtual void checkConfig();
 
    private:
 
@@ -88,9 +110,9 @@ class HmwBrightnessSwitch : public HmwChannel
 
    public:
 
-	  uint8_t triggered;	// read and cleared by linked HmwBrightnessKey
-	  
-	  uint8_t currentValue;	// read by linked HmwBrightnessKey
+      uint8_t triggered;    // read and cleared by linked HmwBrightnessKey
+      
+      uint8_t currentValue;    // read by linked HmwBrightnessKey
 
    private:
 
@@ -101,24 +123,22 @@ class HmwBrightnessSwitch : public HmwChannel
       uint16_t nextActionDelay;
 
       Timestamp lastActionTime;
-	  
-	  //ActionParameter const* actionParameter;
-	  
-	  uint8_t buffer[MAX_SAMPLES] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-	  int16_t result_sum;
-	  uint8_t index;
-	  uint8_t count;
-	  
-	  union tag_state_flags {
-		struct state_flags {
-			uint8_t notUsed :4; // lowest 4 bit are not used, based on XML state_flag definition (index="12.4" size="0.4")
-			uint8_t active  :1; // condition is currently met
-			uint8_t le  :1; // less or equals [le] (false: greater or equals [ge])
-			uint8_t blockingTimeActive  :1;
-			uint8_t unused  :1;
-		} element;
-		uint8_t byte:8;
-	  } stateFlags;
+      
+      uint8_t buffer[MAX_SAMPLES] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+      int16_t result_sum;
+      uint8_t index;
+      uint8_t count;
+      
+      union tag_state_flags {
+        struct state_flags {
+            uint8_t notUsed :4; // lowest 4 bit are not used, based on XML state_flag definition (index="12.4" size="0.4")
+            uint8_t active  :1; // condition is currently met
+            uint8_t le  :1; // less or equals [le] (false: greater or equals [ge])
+            uint8_t blockingTimeActive  :1;
+            uint8_t unused  :1;
+        } element;
+        uint8_t byte:8;
+      } stateFlags;
 };
 
 
